a0f166337f394589bb2ecd664984d3f0c6727973
Grace Mao <grace2017hh@gmail.com>
2021-01-27 11:20:04 -0800

    Added three-layer structure. Mybatis example: http://127.0.0.1:9001/system/test

--- system/src/main/java/com/course/system/SystemApplication.java
+++ system/src/main/java/com/course/system/SystemApplication.java
@@ -1,14 +1,18 @@
 package com.course.system;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ComponentScan("com.course")
+@MapperScan("com.course.system.mapper")
 public class SystemApplication {
 
 	private static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
@@ -16,8 +20,8 @@ public class SystemApplication {
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(SystemApplication.class);
 		Environment env = app.run(args).getEnvironment();
-		LOG.info("run successfully！！");
-		LOG.info("system address: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+		LOG.info("Run successfully！！");
+		LOG.info("System address: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
 	}
 
 }
