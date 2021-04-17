356e6b15aa38b04bc144657feabc7106bef784f6
Grace Mao <grace2017hh@gmail.com>
2021-04-17 01:01:49 -0700

    1.点击sidebar菜单实现页面跳转并激活样式

--- admin/src/views/admin/welcome.vue
+++ admin/src/views/admin/welcome.vue
@@ -3,3 +3,15 @@
     <h1>hello welcome</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: "welcome",
+  mounted: function() {
+    //子组件调用父组件的方法
+    this.$parent.activeSidebar("welcome-sidebar");
+  },
+  methods: {
+  }
+}
+</script>
